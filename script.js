fc5ff9a0a14d61d0175be9b2878427be93caacb2
mikekavouras <mike@wantful.com>
2013-09-08 16:07:23 -0400

    correct audio path

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('#trigger').easyAudio({
-    mpeg: 'http://meaganproctorkavouras.com/audio/pewpew.mp3',
-    wav:  'http://meaganproctorkavouras.com/audio/pewpew.wav',
-    ogg:  'http://meaganproctorkavouras.com/audio/pewpew.ogg'
+    mpeg: 'http://meaganproctorkavouras.com/audio/song.mp3',
+    wav:  'http://meaganproctorkavouras.com/audio/song.wav',
+    ogg:  'http://meaganproctorkavouras.com/audio/song.ogg'
   });
 });
